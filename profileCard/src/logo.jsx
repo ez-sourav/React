b2a2c05bdb5250d5aa8d829931f4237e0a056ecf
ez-sourav <souravb2003@gmail.com>
2025-11-08 20:35:23 +0530

    commit

---
+++ profileCard/src/logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const logo = () => {
+  return (
+    <img src="https://freepngimg.com/download/icon/avatars/5817-man-person.png" alt="" />
+  )
+}
+
+export default logo
