24e331caa167196051a1993cb5d91610d8f73057
ez-sourav <souravb2003@gmail.com>
2025-11-12 15:29:43 +0530

    improve version

--- task-1/src/App.jsx
+++ task-1/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Link } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div className="h-screen bg-gray-900 flex flex-col items-center justify-center text-white space-y-6">
+      <h1 className="text-3xl font-bold">Employee Data Management</h1>
+      <p className="text-gray-300 max-w-md text-center">
+        A simple React project submitting and viewing employee details dynamically using React Router actions.
       </p>
-    </>
-  )
-}
 
-export default App
+      <div className="flex gap-4">
+        <Link
+          to="/inputdata"
+          className="bg-blue-600 px-4 py-2 rounded-md hover:bg-blue-700 transition"
+        >
+          Go to Form
+        </Link>
+        <Link
+          to="/dashboard"
+          className="bg-green-600 px-4 py-2 rounded-md hover:bg-green-700 transition"
+        >
+          View Dashboard
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default App;
